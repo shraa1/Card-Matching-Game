5253a65058af2456f9897a0643bcd20acc311f8f
Shraavan Acharya <shraavan.acharya@gmail.com>
2025-08-23 22:04:42 +0530

    Added GameInfo ScriptableObject

--- Assets/Scripts/Models/GameInfo.cs
+++ Assets/Scripts/Models/GameInfo.cs
@@ -13,7 +13,37 @@ namespace Shraa1.CardGame.Models {
 		/// Front Card Sprites
 		/// </summary>
 		[SerializeField] private Sprite[] m_FrontCardSprites;
+		/// <summary>
+		/// Front Card Sprites
+		/// </summary>
 		public Sprite[] FrontCardSprite { get => m_FrontCardSprites; }
+
+		/// <summary>
+		/// Back Card Face Sprite
+		/// </summary>
+		[SerializeField] private Sprite m_BackCardSprite;
+		/// <summary>
+		/// Back Card Face Sprite
+		/// </summary>
+		public Sprite BackCardSprite { get => m_BackCardSprite; }
+
+		/// <summary>
+		/// Grid Size minimum. If 2x2 is the minimum grid, it should be specified here so the user can select the relevant between GridSizeMin and GridSizeMax
+		/// </summary>
+		[SerializeField] private Vector2Int m_GridSizeMin;
+		/// <summary>
+		/// Grid Size minimum. If 2x2 is the minimum grid, it should be specified here so the user can select the relevant between GridSizeMin and GridSizeMax
+		/// </summary>
+		public Vector2Int GridSizeMin { get => m_GridSizeMin; }
+
+		/// <summary>
+		/// Grid Size maximum. If 3x6 is the minimum grid, it should be specified here so the user can select the relevant between GridSizeMin and GridSizeMax
+		/// </summary>
+		[SerializeField] private Vector2Int m_GridSizeMax;
+		/// <summary>
+		/// Grid Size maximum. If 3x6 is the minimum grid, it should be specified here so the user can select the relevant between GridSizeMin and GridSizeMax
+		/// </summary>
+		public Vector2Int GridSizeMax { get => m_GridSizeMax; }
 		#endregion Properties
 	}
 }
